fcb724f089ff7dceb98031fd7ef26ca6ce014225
luoy <luoy@localhost.localdomain>
2013-08-27 08:40:40 +0800

    add searchengine.py

---
+++ python/search/searchengine.py
@@ -0,0 +1,23 @@
+class Crawler:
+    def __init__(self, dbname):
+        pass
+
+    def __del__(self):
+        pass
+
+    def dbcommit(self):
+        pass
+
+    def getentryid(self, table, field, value, createnew = True):
+        return None
+
+    def addtoindex(self, url, soup):
+        print 'Indexing %s' % url
+
+    def gettextonly(self, soup):
+        return None
+
+    def separatewords(self, text):
+        return None
+
+
