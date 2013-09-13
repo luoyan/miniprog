d95f40935371cf0f418ed2ec70f4281bbfec1159
luoyan <luoyan@maimiaotech.com>
2013-09-13 16:03:29 +0800

    add extra

--- django/op.sh
+++ django/op.sh
@@ -1,10 +1,20 @@
+function usage()
+{
+    echo "$0 create site"
+    echo "$0 start site"
+}
+if [ $# -ne 2 ] ; then
+    usage $0
+    exit -1
+fi
+site=$2
 create()
 {
-    django-admin.py startproject mysite
+    django-admin.py startproject $site
 }
 start()
 {
-    cd mysite
+    cd $site
     python manage.py runserver 0.0.0.0:8080
 }
 $@
