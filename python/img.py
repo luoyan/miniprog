33de0f2cc3885768ff30434be08088a2ce2c3596
luoyan <luoyan@maimiaotech.com>
2013-11-20 11:38:24 +0800

    add img.py

---
+++ python/img.py
@@ -0,0 +1,16 @@
+__author__ = 'gaonan'
+#coding=utf-8
+import Image
+import sys
+
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    im = Image.open(infile)
+    (x,y) = im.size #read image size
+    x_s = 100 #define standard width
+    y_s = y * x_s / x #calc height based on standard width
+    out = im.resize((x_s,y_s),Image.ANTIALIAS) #resize image with high-quality
+    out.save(outfile)
+    print 'original size: ',x,y
+    print 'adjust size: ',x_s,y_s
