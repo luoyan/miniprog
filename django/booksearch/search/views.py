6378367ebd4d1ffd718eb386030d070b83fa2338
luoyan <luoyan5@163.com>
2014-01-24 22:27:43 +0800

    add template for doubansearch

--- django/booksearch/search/views.py
+++ django/booksearch/search/views.py
@@ -20,16 +20,32 @@ from django.template import Template, Context
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
+from django import template
 # Create your views here.
 def home(request):
     response = render_to_response("index.html",{}, context_instance=RequestContext(request))
     return response
 
 def index(request):
-    t = get_template('booksearch.html')
-    html = t.render(RequestContext(request))
+    #t = get_template('bookindex.html')
+    t = get_template('booksearch2.html')
+    #t = get_template('booksearch.html')
+    #html = t.render(RequestContext(request))
+    #return HttpResponse(html)
+    item = {}
+    item['author'] = u'叶萱'
+    item['diguest'] = u'《你在我的左手边》内容简介：隐约，还是可以记起一张脸，那样白皙的皮肤、清秀的面容，目光里有深深的冷。伴随着那样鄙弃、不屑的声音，响彻我的记忆：“陶滢，你不漂...'
+    item['img_url'] = 'http://img5.douban.com/mpic/s8460999.jpg'
+    item['price'] = 22
+    item['rating'] = 7.6
+    item['title'] = u'你在我的左手边'
+    item['url'] = 'http://book.douban.com/subject/3987275/'
+    book_items = []
+    book_items.append(item)
+    c = template.Context({'book_items': book_items})
+    html = t.render(c)
     return HttpResponse(html)
-    #response = render_to_response("booksearch.html",{}, context_instance=RequestContext(request))
+    #response = render_to_response("bookindex.html",{'book_items':book_items}, context_instance=RequestContext(request))
     #return response
 
 def images_files(request):
