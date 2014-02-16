10f897c0b12c27b4a4153bcb0a61f3fb778e8503
luoyan <luoyan@maimiaotech.com>
2014-02-16 20:02:54 +0800

    add redis_client.py

---
+++ redis/redis_client.py
@@ -0,0 +1,15 @@
+from redis import Redis
+redisConn = Redis(host='redis1', port=6379, db=1)
+#key='hello'
+key=1
+'''
+value='world'
+value={'1':'world'}
+value=['1', 'world']
+value={'1': {'2': 'world'}}
+value={'1': {'2': ['hello', 'world']}}
+redisConn.set(key, value)
+'''
+value2 = redisConn.get(key)
+redisConn.save()
+print 'value ' + str(value2)
