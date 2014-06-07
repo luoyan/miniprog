aceaf85ff07788a3b37382a332e3da286c13452f
fool <fool@fooldeMacBook-Pro.local>
2014-06-08 02:08:42 +0800

    add first ios App

--- ios/HelloWorld/HelloWorld/ViewController.m
+++ ios/HelloWorld/HelloWorld/ViewController.m
@@ -18,6 +18,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    int w = self.view.frame.size.width;
+    int h = self.view.frame.size.height;
+    
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
+    
+    label.center = CGPointMake(w/2, h/2);
+    label.textAlignment = UITextAlignmentCenter;
+    label.backgroundColor = [UIColor clearColor];
+    label.text = @"Hello World!";
+    
+    [self.view addSubview:label];
+    
+    //[label release];
 }
 
 - (void)didReceiveMemoryWarning
