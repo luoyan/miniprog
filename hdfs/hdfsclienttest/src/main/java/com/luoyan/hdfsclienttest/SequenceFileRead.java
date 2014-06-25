8787b62979795e29bf884cea0d144f528cfe0131
luoyan <luoyan5@163.com>
2014-06-25 16:59:05 +0800

    add SequenceFileRead

---
+++ hdfs/hdfsclienttest/src/main/java/com/luoyan/hdfsclienttest/SequenceFileRead.java
@@ -0,0 +1,42 @@
+package com.luoyan.hdfsclienttest;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.BytesWritable;
+
+import java.io.IOException;
+
+public class SequenceFileRead {
+	public void read(Configuration conf, FileSystem fs, Path path) throws IOException {
+		SequenceFile.Reader reader = new SequenceFile.Reader(fs, path, conf);
+
+		BytesWritable key = new BytesWritable();
+		BytesWritable val = new BytesWritable();
+
+		while (reader.next(key, val)) {
+			System.err.println("key : " + key + "\t" + " value : " + val);
+		}
+
+		reader.close();
+	}
+	public void ReadLocal(String path) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.getLocal(conf);
+		Path seqFilePath = new Path(path);
+		read(conf, fs, seqFilePath);
+	}
+	public static void main(String[] args) throws IOException {
+		SequenceFileRead sfr = new SequenceFileRead();
+		if (args.length < 1) {
+			System.err.println("command path(file key=BytesWritable value=BytesWritable)");
+			System.exit(-1);
+		}
+		String path = args[0];
+		sfr.ReadLocal(path);
+	}
+
+}
