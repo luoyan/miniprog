f9f3518132d072343f6c7c291c19d9742b7d8d8f
luoyan <luoyan@maimiaotech.com>
2014-07-09 16:16:08 +0800

    add FreeMarker

---
+++ java/syntax/src/main/java/com/luoyan/syntax/FreeMarkerObject.java
@@ -0,0 +1,19 @@
+package com.luoyan.syntax;
+
+public class FreeMarkerObject {
+	  private String name;
+	  private String developer;
+
+	  public FreeMarkerObject(String name, String developer) {
+	    this.name = name;
+	    this.developer = developer;
+	  }
+
+	  public String getName() {
+	    return name;
+	  }
+
+	  public String getDeveloper() {
+	    return developer;
+	  }
+}
---
+++ java/syntax/src/main/java/com/luoyan/syntax/FreeMarkerTest.java
@@ -0,0 +1,75 @@
+package com.luoyan.syntax;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+
+public class FreeMarkerTest {
+	public static void main(String[] args) throws Exception {
+
+		// 1. Configure FreeMarker
+		//
+		// You should do this ONLY ONCE, when your application starts,
+		// then reuse the same Configuration object elsewhere.
+
+		Configuration cfg = new Configuration();
+
+		// Where do we load the templates from:
+		//cfg.setClassForTemplateLoading(FreeMarkerTest.class, "templates");
+
+		// Some other recommended settings:
+		cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setLocale(Locale.US);
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+		// 2. Proccess template(s)
+		//
+		// You will do this for several times in typical applications.
+
+		// 2.1. Prepare the template input:
+
+		Map<String, Object> input = new HashMap<String, Object>();
+
+		input.put("title", "Vogella example");
+
+		input.put("exampleObject", new FreeMarkerObject("Java object", "me"));
+
+		List<FreeMarkerObject> systems = new ArrayList<FreeMarkerObject>();
+		systems.add(new FreeMarkerObject("Android", "Google"));
+		systems.add(new FreeMarkerObject("iOS States", "Apple"));
+		systems.add(new FreeMarkerObject("Ubuntu", "Canonical"));
+		systems.add(new FreeMarkerObject("Windows7", "Microsoft"));
+		input.put("systems", systems);
+
+		// 2.2. Get the template
+
+		Template template = cfg.getTemplate("helloworld.ftl");
+
+		// 2.3. Generate the output
+
+		// Write output to the console
+		Writer consoleWriter = new OutputStreamWriter(System.out);
+		template.process(input, consoleWriter);
+
+		// For the sake of example, also write output into a file:
+		Writer fileWriter = new FileWriter(new File("output.html"));
+		try {
+			template.process(input, fileWriter);
+		} finally {
+			fileWriter.close();
+		}
+
+	}
+}
