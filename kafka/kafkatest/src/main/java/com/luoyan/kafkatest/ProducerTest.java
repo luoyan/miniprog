7cfbdf68e8759979e2d270e35e51ab161c50a1cd
luoyan <luoyan5@163.com>
2014-07-26 18:34:53 +0800

    add messageNum

--- kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ProducerTest.java
+++ kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ProducerTest.java
@@ -19,22 +19,23 @@ public class ProducerTest {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger("ProducerTest");
 	private static void usage() {
-		System.err.println("command topicName brokerList=localhost:9092");
+		System.err.println("command topicName brokerList=localhost:9092 messageNum");
 	}
 	public static void main(String[] args) {
-		if (args.length != 2) {
+		if (args.length != 3) {
 			usage();
 			System.exit(-1);
 		}
 		String topicName = args[0];
 		String brokers = args[1];
+		int messageNum = Integer.parseInt(args[2]);
 		Properties props = new Properties();
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		props.put("metadata.broker.list", brokers);
 		props.put("request.required.acks", "1");
 		ProducerConfig config = new ProducerConfig(props);
 		Producer<String, String> producer = new Producer<String, String>(config);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < messageNum; i++) {
 			producer.send(new KeyedMessage<String, String>(topicName, "test" + i));
 			// System.out.println("send topic test [test" + i + "]");
 			LOGGER.debug("send topic test2 [test" + i + "]");
