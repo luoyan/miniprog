f8dc05b8bf5792835485e9a6eaca2bcba055aae3
luoyan <luoyan5@163.com>
2014-07-26 17:50:24 +0800

    more flexible for kafkatest

--- kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ConsumerTest.java
+++ kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ConsumerTest.java
@@ -1,56 +1,68 @@
 package com.luoyan.kafkatest;
 
-import java.util.HashMap;  
-import java.util.List;  
-import java.util.Map;  
-import java.util.Properties;  
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import kafka.consumer.ConsumerConfig;  
-import kafka.consumer.ConsumerIterator;  
-import kafka.consumer.KafkaStream;  
-import kafka.javaapi.consumer.ConsumerConnector;  
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
 
-public class ConsumerTest extends Thread {  
-        private final ConsumerConnector consumer;  
-        private final String topic;  
-        private static final Logger LOGGER = LoggerFactory.getLogger("ConsumerTest");
+public class ConsumerTest extends Thread {
+	private final ConsumerConnector consumer;
+	private final String topic;
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger("ConsumerTest");
+	private static String zookeeperAddress;
+	private static String groupId;
+	private static void usage() {
+		System.err.println("command topic_name zookeeper/chroot group.id");
+	}
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			usage();
+			System.exit(-1);
+		}
+		String topicName = args[0];
+		zookeeperAddress = args[1];
+		groupId = args[2];
+		ConsumerTest consumerThread = new ConsumerTest(topicName);
+		consumerThread.start();
+	}
 
-        public static void main(String[] args) {  
-                ConsumerTest consumerThread = new ConsumerTest("test2");  
-                consumerThread.start();  
-        }  
+	public ConsumerTest(String topic) {
+		consumer = kafka.consumer.Consumer
+				.createJavaConsumerConnector(createConsumerConfig());
+		this.topic = topic;
+	}
 
-        public ConsumerTest(String topic) {  
-                consumer = kafka.consumer.Consumer  
-                        .createJavaConsumerConnector(createConsumerConfig());  
-                this.topic = topic;  
-        }  
+	private static ConsumerConfig createConsumerConfig() {
+		Properties props = new Properties();
+		props.put("zookeeper.connect", zookeeperAddress);
+		props.put("group.id", groupId);
+		props.put("zookeeper.session.timeout.ms", "400000");
+		props.put("zookeeper.sync.time.ms", "200");
+		props.put("auto.commit.interval.ms", "1000");
 
-        private static ConsumerConfig createConsumerConfig() {  
-                Properties props = new Properties();  
-                props.put("zookeeper.connect", "localhost:2181/kafka/lgprc-xiaomi");  
-                props.put("group.id", "2");  
-                props.put("zookeeper.session.timeout.ms", "400000");  
-                props.put("zookeeper.sync.time.ms", "200");  
-                props.put("auto.commit.interval.ms", "1000");  
+		return new ConsumerConfig(props);
 
-                return new ConsumerConfig(props);  
+	}
 
-        }  
-
-        public void run() {  
-                Map<String, Integer> topicCountMap = new HashMap<String, Integer>();  
-                topicCountMap.put(topic, new Integer(1));  
-                Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer  
-                        .createMessageStreams(topicCountMap);  
-                KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);  
-                ConsumerIterator<byte[], byte[]> it = stream.iterator();  
-                LOGGER.debug("start loop");
-                while (it.hasNext())  
-                        //LOGGER.debug("hasNext");
-                        //System.out.println(new String(it.next().message()));  
-                        LOGGER.debug(new String(it.next().message()));  
-                LOGGER.debug("end loop");
-        }  
-}  
+	public void run() {
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
+				.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+		LOGGER.debug("start loop");
+		while (it.hasNext())
+			// LOGGER.debug("hasNext");
+			// System.out.println(new String(it.next().message()));
+			LOGGER.debug(new String(it.next().message()));
+		LOGGER.debug("end loop");
+	}
+}
--- kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ProducerTest.java
+++ kafka/kafkatest/src/main/java/com/luoyan/kafkatest/ProducerTest.java
@@ -1,34 +1,44 @@
 package com.luoyan.kafkatest;
 
-import java.util.Properties;  
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import kafka.javaapi.producer.Producer;  
-import kafka.producer.KeyedMessage;  
-import kafka.producer.ProducerConfig;  
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 
-/** 
- * Test the Kafka Producer 
- * @author luoyan 
+/**
+ * Test the Kafka Producer
  * 
- */  
-public class ProducerTest { 
-        private static final Logger LOGGER = LoggerFactory.getLogger("ProducerTest");
-        public static void main(String[] args) {  
-                Properties props = new Properties();  
-                //props.put("zk.connect", "localhost:2181/kafak");  
-                props.put("serializer.class", "kafka.serializer.StringEncoder");  
-                props.put("metadata.broker.list", "localhost:9092"); 
-                props.put("request.required.acks", "1");
-                ProducerConfig config = new ProducerConfig(props);  
-                Producer<String, String> producer = new Producer<String, String>(config);  
-                for (int i = 0; i < 10; i++)  {
-                        producer.send(new KeyedMessage<String, String>("test2", "test" + i));  
-                        //System.out.println("send topic test [test" + i + "]");
-                        LOGGER.debug("send topic test2 [test" + i + "]");
-                }
+ * @author luoyan
+ * 
+ */
+public class ProducerTest {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger("ProducerTest");
+	private static void usage() {
+		System.err.println("command topicName brokerList=localhost:9092");
+	}
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			usage();
+			System.exit(-1);
+		}
+		String topicName = args[0];
+		String brokers = args[1];
+		Properties props = new Properties();
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		props.put("metadata.broker.list", brokers);
+		props.put("request.required.acks", "1");
+		ProducerConfig config = new ProducerConfig(props);
+		Producer<String, String> producer = new Producer<String, String>(config);
+		for (int i = 0; i < 10; i++) {
+			producer.send(new KeyedMessage<String, String>(topicName, "test" + i));
+			// System.out.println("send topic test [test" + i + "]");
+			LOGGER.debug("send topic test2 [test" + i + "]");
+		}
 
-        }  
-}  
+	}
+}
