af1ddc3823a3bfa5e5441c06eb9f8e1fb32a49cb
luoyan <luoyan5@163.com>
2014-08-09 17:27:48 +0800

    add hchart ajax sample

--- java/ajax-sample/src/main/java/com/xiaomi/miui/ad/webpages/controllers/DemoController.java
+++ java/ajax-sample/src/main/java/com/xiaomi/miui/ad/webpages/controllers/DemoController.java
@@ -35,8 +35,24 @@ public class DemoController {
     @Get("get_json")
     public String get_json() {
     	Model model = inv.getModel();
-    	String json = "[{\"firstName\":\"Bill\",\"lastName\":\"Gates\",\"age\":60}]";
+    	String json = "{\"firstName\":\"Bill\",\"lastName\":\"Gates\",\"age\":60}";
     	model.add("data", json);
+    	return "@" + json;
+    }
+    
+    @Get("get_json_start")
+    public String get_json_index() {
     	return "get_json";
+    }
+    
+    @Get("line_labels")
+    public String line_labels() {
+    	return "line-labels";
+    }
+    
+    @Get("get_data")
+    public String get_data() {
+    	String json = "{\"data1\":[7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6], \"data2\":[3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]}";
+    	return "@" + json;
     }
 }
