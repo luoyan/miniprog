d4c26286c4a4ff4811cd41a780f866018458179d
luoyan <luoyan5@163.com>
2016-03-30 10:26:46 +0800

    remove syntaxxiaomi

--- android/CheckIn/src/com/luoyan/checkin/MainActivity.java
+++ android/CheckIn/src/com/luoyan/checkin/MainActivity.java
@@ -1,5 +1,10 @@
 package com.luoyan.checkin;
 
+import com.zxinsight.MWConfiguration;
+import com.zxinsight.MWImageView;
+import com.zxinsight.MagicWindowSDK;
+import com.zxinsight.TrackAgent;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -20,13 +25,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MWConfiguration config = new MWConfiguration(this);
+        config.setChannel("yiqiwan").setDebugModel(true);
+        MagicWindowSDK.initSDK(this);
+        MWImageView imageView = (MWImageView) findViewById(R.id.mw_banner);
+        imageView.bindEvent("H3LTFWGM");
         Bundle b = getIntent().getExtras();
         String url = b.getString("url");
         setContentView(R.layout.webview);  
         webView = (WebView) findViewById(R.id.webView1);  
           
-        webView.getSettings().setJavaScriptEnabled(true);//����ʹ�ù�ִ��JS�ű�  
-        webView.getSettings().setBuiltInZoomControls(true);//����ʹ֧������  
+        webView.getSettings().setJavaScriptEnabled(true);//锟斤拷锟斤拷使锟矫癸拷执锟斤拷JS锟脚憋拷  
+        webView.getSettings().setBuiltInZoomControls(true);//锟斤拷锟斤拷使支锟斤拷锟斤拷锟斤拷  
 //      webView.getSettings().setDefaultFontSize(5);
         
         webView.loadUrl(url);
@@ -34,10 +44,10 @@ public class MainActivity extends Activity {
             @Override  
             public boolean shouldOverrideUrlLoading(WebView view, String url) {  
                 // TODO Auto-generated method stub  
-                view.loadUrl(url);// ʹ�õ�ǰWebView������ת  
-                return true;//true��ʾ���¼��ڴ˴�����������Ҫ�ٹ㲥  
+                view.loadUrl(url);// 使锟矫碉拷前WebView锟斤拷锟斤拷锟斤拷转  
+                return true;//true锟斤拷示锟斤拷锟铰硷拷锟节此达拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷要锟劫广播  
             }  
-            @Override   //ת�����ʱ�Ĵ���  
+            @Override   //转锟斤拷锟斤拷锟绞憋拷拇锟斤拷锟� 
             public void onReceivedError(WebView view, int errorCode,  
                     String description, String failingUrl) {  
                 // TODO Auto-generated method stub  
@@ -45,7 +55,18 @@ public class MainActivity extends Activity {
             }  
         });
     }
+    
+    @Override
+    protected void onPause() {
+        TrackAgent.currentEvent().onPause(this);
+        super.onPause();
+    }
 
+    @Override
+    protected void onResume() {
+        TrackAgent.currentEvent().onResume(this);
+        super.onResume();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,7 +104,7 @@ public class MainActivity extends Activity {
         }
     }
     
-    @Override   //Ĭ�ϵ���˼������˳�Activity�����������������ʹ������WebView�ڷ���  
+    @Override   //默锟较碉拷锟斤拷思锟斤拷锟斤拷锟斤拷顺锟紸ctivity锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟绞癸拷锟斤拷锟斤拷锟絎ebView锟节凤拷锟斤拷  
     public boolean onKeyDown(int keyCode, KeyEvent event) {  
         // TODO Auto-generated method stub  
         if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {  
--- java/syntaxxiaomi/thrift/MiuiLogCommon.thrift
+++
@@ -1,7 +0,0 @@
-namespace java com.xiaomi.miui.ad.thrift.model
-
-struct MiuiLogScribeInfo {
-    1: string scribeInfo;
-    2: string time;
-    3: optional string ip;
-}
